75f3c204bf09712a3d241d6a0e882c1ebfda4894
Garrett Gordon <gagordon@mit.edu>
2022-06-21 14:35:58 -0400

    switching to production

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-const DEVELOPMENT = true
+const DEVELOPMENT = false
 const DEVELOPMENT_FRONTEND = "http://localhost:3000"
-const PRODUCTION_FRONTEND = null
+const PRODUCTION_FRONTEND = "https://friendify-frontend.herokuapp.com"
 
 module.exports = {
   getOrigin : () => {return DEVELOPMENT? DEVELOPMENT_FRONTEND : PRODUCTION_FRONTEND}
